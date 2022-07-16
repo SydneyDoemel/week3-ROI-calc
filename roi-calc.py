1f1f40e2584ff9ca6da2545b83f953e9a17df63d
Sydney C Doemel Doemel <sydneycdoemeldoemel@Sydneys-MacBook-Pro.local>
2022-07-16 10:49:55 -0400

    secondcommit

--- roi-calc.py
+++ roi-calc.py
@@ -5,43 +5,80 @@ class RoiCalc:
         self.cash_flow = None
         self.total_investment = None
         self.roi = None
+       
+
+#THE BELOW DIDNT WORK, BUTT TRY  SOMETING ELSE
+    def test_digit(self, x, my_string):
+        valid = False
+        while not valid: #loop until the user enters a valid int
+            try:
+                self.x = int(input(self.my_string))
+                valid = True #if this point is reached, x is a valid int
+            except ValueError:
+                print('Please only input digits')
+
+
+
+                
     def income(self):
         input("To begin calculating your cash on cash ROI, press enter: ")
-        active  = True
-        while active:                                                                           #This is repeated below.
-            self.rental_income_month = input("Enter your total monthly rental income: $")       # Should I just
-            if self.rental_income_month.isdigit() != True:                                      # have it call back
-                print("Please enter only digits.")                                              #instead of repeating
-            else:                                                                               #the whole thing
-                self.rental_income_month = int(self.rental_income_month)                        #again
-                active  = False                                                                 #?
+        valid = False
+        while not valid: #loop until the user enters a valid int
+            try:
+                self.rental_income_month = int(input("Enter your total monthly rental income: $"))
+                valid = True #if this point is reached, x is a valid int
+            except ValueError:
+                print('Please only input digits')                                                                                   
+                                                                                             
         #question about miscelannious or no?
+        
+        
+
+
+
         while True:
             go_back = input(f"Your total monthly income from this rental property is ${self.rental_income_month}. Press Y to confirm or N to change: ")
             if go_back.lower() == "y":
                 return self.expenses()
             elif go_back.lower() == "n":
-                active  = True
-                while active:
-                    self.rental_income_month = input("Enter your total monthly rental income: $")
-                    if self.rental_income_month.isdigit() != True:
-                        print("Please enter only digits.")
-                    else:
-                        self.rental_income_month = int(self.rental_income_month)
-                        active  = False
+                valid = False
+                while not valid: #loop until the user enters a valid int
+                    try:
+                        self.rental_income_month = int(input("Enter your total monthly rental income: $"))
+                        valid = True #if this point is reached, x is a valid int
+                    except ValueError:
+                        print('Please only input digits')
             else:
                 print("Invalid input")
 
             
     def expenses(self):
         expense_lst = {}
-        expense_lst["tax"] = int(input("Enter your monthly tax expense: $"))
+
+        #expense_lst["tax"] = int(input("Enter your monthly tax expense: $"))
+        valid = False
+        while not valid:
+            try:
+                expense_lst["tax"] = int(input("Enter your monthly tax expense: $"))
+                valid = True 
+            except ValueError:
+                 print('Please only input digits')
         
-        expense_lst["insurance"] = int(input("Enter your insurance tax expense: $"))
-      
+        
+        
+        #expense_lst["insurance"] = int(input("Enter your insurance tax expense: $"))
+        valid = False
+        while not valid:
+            try:
+                expense_lst["insurance"] = int(input("Enter your insurance tax expense: $"))
+                valid = True 
+            except ValueError:
+                 print('Please only input digits')
+
+
         expense_lst["utilities"] = int(input("Enter your monthly utilities expense, if not paid by tenant: $"))
         
-        expense_lst["hoa"] = int(input(input("Enter your monthly HOA fee: $")))
+        expense_lst["hoa"] = int(input("Enter your monthly HOA fee: $"))
        
         expense_lst["lawn"] = int(input("Enter your monthly lawn/snow expense: $"))
        
@@ -54,7 +91,11 @@ class RoiCalc:
         expense_lst["prop_man"] = int(input("Enter your monthly property management expense: $"))
        
         expense_lst["mortgage"] = int(input("Enter your monthly mortgage payment: $"))
-        print(expense_lst)
+
+
+
+        
+        print([f"{k}:{v}" for k,v in expense_lst.items()]) # how can i get this to print if not a list
         self.total_expenses = sum(expense_lst.values())
         print(f"Your total monthly expenses: ${self.total_expenses}.")
         self.cash_flow = self.rental_income_month - self.total_expenses
@@ -64,21 +105,21 @@ class RoiCalc:
     def return_on(self):
         invest_dict = {}
         input("Press enter to calculate your total investment: ")
-        down = input("Enter your down payment on this property: $")
-        invest_dict["Down payment"] = int(down)
-        closing = input("Enter your closing costs on this property: $")
-        invest_dict["Closing cost"] = int(closing)
-        rehab = input("Enter your rehab costs on this property: $")
-        invest_dict["Rehab costs"] = int(rehab)
-        misc = input("Enter any misc. costs on this property: $")
-        invest_dict["Misc. costs"] = int(misc)
+        
+        invest_dict["Down payment"] = int(input("Enter your down payment on this property: $"))
+        
+        invest_dict["Closing cost"] = int(input("Enter your closing costs on this property: $"))
+       
+        invest_dict["Rehab costs"] = int(input("Enter your rehab costs on this property: $"))
+        
+        invest_dict["Misc. costs"] = int(input("Enter any misc. costs on this property: $"))
         print(invest_dict)
         self.total_investment = sum(invest_dict.values())
         print(f"Your total investment is ${self.total_investment}")
         self.roi = ((self.cash_flow *12) /(self.total_investment)) * 10
-        print(f"Your cash on cash ROI is {self.roi}%.")
+        print(f"Your cash on cash ROI is {round(self.roi, 3)}%.")
 
 
 new = RoiCalc()
+
 new.income()
-new.expenses()
